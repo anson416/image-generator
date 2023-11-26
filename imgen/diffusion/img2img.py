2831571a91e6051c490a000dc308b1dfc4bf6a2a
Anson416 <lamyiufung2003@gmail.com>
2023-11-26 19:08:08 +0800

    Implement Control Net

--- imgen/diffusion/img2img.py
+++ imgen/diffusion/img2img.py
@@ -5,7 +5,9 @@ from typing import Any, List, Optional, Union
 
 import cv2
 import numpy as np
-from diffusers import StableDiffusionImg2ImgPipeline
+from diffusers import (ControlNetModel,
+                       StableDiffusionControlNetImg2ImgPipeline,
+                       StableDiffusionImg2ImgPipeline)
 from PIL import Image
 from utils.date_time import get_datetime
 from utils.types_ import PathLike
@@ -19,14 +21,15 @@ class SDImage2Image(StableDiffusion_):
             StableDiffusionImg2ImgPipeline,
             **kwargs,
         )
+        self.initialize()
     
     def __call__(
         self,
         *,
-        img_path: Optional[PathLike] = None,
         img: Optional[Union[Image.Image, np.ndarray]] = None,
+        img_path: Optional[PathLike] = None,
         prompt: Optional[str] = None,
-        neg_prompt: Optional[str] = None,
+        negative_prompt: Optional[str] = None,
         n_imgs: int = 1,
         output_dir: Optional[PathLike] = None,
         n_steps: int = 50,
@@ -35,13 +38,11 @@ class SDImage2Image(StableDiffusion_):
         seed: Optional[int] = None,
         **kwargs: Any,
     ) -> List[Image.Image]:
-        assert img_path or img, "img_path and img cannot be both None"
-
         return super().__call__(
             output_dir=output_dir,
-            image=self.open_img(img_path) if img_path else img,
+            image=self.load_img(img=img, img_path=img_path),
             prompt=self.get_positive_prompt(prompt),
-            negative_prompt=self.get_negative_prompt(neg_prompt),
+            negative_prompt=self.get_negative_prompt(negative_prompt),
             num_images_per_prompt=n_imgs,
             num_inference_steps=n_steps,
             strength=strength,
@@ -49,6 +50,70 @@ class SDImage2Image(StableDiffusion_):
             generator=self.get_generator(seed=seed),
             **kwargs,
         )
+
+
+class ControlSDImage2Image(StableDiffusion_):
+    def __init__(self, **kwargs: Any) -> None:
+        super().__init__(
+            StableDiffusionControlNetImg2ImgPipeline,
+            **kwargs,
+        )
+        self.pipe.controlnet = ControlNetModel.from_pretrained(
+            "lllyasviel/sd-controlnet-canny",
+            torch_dtype=self.torch_dtype,
+            cache_dir=self.model_dir,
+        )
+        self.initialize()
+
+    def __call__(
+        self,
+        *,
+        img: Optional[Union[Image.Image, np.ndarray]] = None,
+        img_path: Optional[PathLike] = None,
+        lower_threshold: float = 100,
+        upper_threshold: float = 200,
+        prompt: Optional[str] = None,
+        negative_prompt: Optional[str] = None,
+        n_imgs: int = 1,
+        output_dir: Optional[PathLike] = None,
+        n_steps: int = 50,
+        strength: float = 0.8,
+        guidance_scale: float = 7.5,
+        seed: Optional[int] = None,
+        **kwargs: Any,
+    ) -> List[Image.Image]:
+        image = self.load_img(img=img, img_path=img_path)
+        
+        return super().__call__(
+            output_dir=output_dir,
+            image=image,
+            control_image=self.get_canny_img(
+                img=image,
+                lower_threshold=lower_threshold,
+                upper_threshold=upper_threshold,
+            ),
+            prompt=self.get_positive_prompt(prompt),
+            negative_prompt=self.get_negative_prompt(negative_prompt),
+            num_images_per_prompt=n_imgs,
+            num_inference_steps=n_steps,
+            strength=strength,
+            guidance_scale=guidance_scale,
+            generator=self.get_generator(seed=seed),
+            **kwargs,
+        )
+    
+    @staticmethod
+    def get_canny_img(
+        img: Optional[Union[Image.Image, np.ndarray]] = None,
+        img_path: Optional[PathLike] = None,
+        lower_threshold: float = 100,
+        upper_threshold: float = 200,
+    ) -> Image.Image:
+        canny_img = StableDiffusion_.load_img(img=img, img_path=img_path)
+        canny_img = np.array(canny_img)
+        canny_img = cv2.Canny(canny_img, lower_threshold, upper_threshold)[:, :, None]
+        canny_img = np.concatenate([canny_img, canny_img, canny_img], axis=2)
+        return Image.fromarray(canny_img)
 
 
 def video2video(
@@ -70,7 +135,6 @@ def video2video(
         if not video_writer:
             video_writer = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"mp4v"), fps, frame.size)
         video_writer.write(np.array(pipe(
-            img_path=None,
             img=frame,
             n_imgs=1,
             output_dir=None,
