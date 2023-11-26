a8d671476c7bb08b4816ffdcc639fc4793e4247b
Anson416 <lamyiufung2003@gmail.com>
2023-11-26 19:07:59 +0800

    Add more models (particularly SDXL)

--- imgen/diffusion/model.py
+++ imgen/diffusion/model.py
@@ -52,11 +52,12 @@ _SD_MODELS = {
     "Ghibli Diffusion": SDModel("Ghibli Diffusion", "nitrosocke/Ghibli-Diffusion", "ghibli style", "https://huggingface.co/nitrosocke/Ghibli-Diffusion"),
     "Openjourney": SDModel("Openjourney", "prompthero/openjourney", "mdjrny-v4 style", "https://huggingface.co/prompthero/openjourney"),
     "SD V1.5": SDModel("Stable Diffusion V1.5", "runwayml/stable-diffusion-v1-5", "", "https://huggingface.co/runwayml/stable-diffusion-v1-5"),
-    "SD V2.0": SDModel("Stable Diffusion V2.0", "stabilityai/stable-diffusion-2", "", "https://huggingface.co/stabilityai/stable-diffusion-2"),
-    "SD V2.1": SDModel("Stable Diffusion V2.1", "stabilityai/stable-diffusion-2-1", "", "https://huggingface.co/stabilityai/stable-diffusion-2-1"),
+    "SD V2 Base": SDModel("Stable Diffusion V2 Base", "stabilityai/stable-diffusion-2-base", "", "https://huggingface.co/stabilityai/stable-diffusion-2-base"),
+    "SD V2-1": SDModel("Stable Diffusion V2-1", "stabilityai/stable-diffusion-2-1", "", "https://huggingface.co/stabilityai/stable-diffusion-2-1"),
     "SD x2 Latent Upscaler": SDModel("Stable Diffusion x2 Latent Upscaler", "stabilityai/sd-x2-latent-upscaler", "", "https://huggingface.co/stabilityai/sd-x2-latent-upscaler"),
     "SD x4 Upscaler": SDModel("Stable Diffusion x4 Upscaler", "stabilityai/stable-diffusion-x4-upscaler", "", "https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler"),
-    "SDXL Base V1.0": SDModel("Stable Diffusion XL Base V1.0", "stabilityai/stable-diffusion-xl-base-1.0", "", "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0"),
+    "SDXL Base V1.0": SDModel("Stable Diffusion XL Base V1.0", "stabilityai/stable-diffusion-xl-refiner-1.0", "", "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0"),
+    "SDXL Refiner V1.0": SDModel("Stable Diffusion XL Refiner V1.0", "stabilityai/stable-diffusion-xl-base-1.0", "", "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0"),
     "Waifu Diffusion": SDModel("Waifu Diffusion", "hakurei/waifu-diffusion", "", "https://huggingface.co/hakurei/waifu-diffusion"),
 }
 
