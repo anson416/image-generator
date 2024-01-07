d340f92c4b5adcf530e460632f5ae6b6d633d512
Anson416 <lamyiufung2003@gmail.com>
2024-01-08 02:49:38 +0800

    Add more models

--- imgen/diffusion/model.py
+++ imgen/diffusion/model.py
@@ -11,53 +11,169 @@ class SDModel(object):
         self,
         name: str,
         path: PathLike,
-        prefix: Optional[str] = None,
+        positive_prefix: Optional[str] = None,
+        negative_prefix: Optional[str] = None,
         website: Optional[str] = None,
     ) -> None:
         self._name = name
         self._path = path
-        self._prefix = prefix
+        self._positive_prefix = positive_prefix
+        self._negative_prefix = negative_prefix
         self._website = website
 
     def __repr__(self) -> str:
         return f"SDModel({self.name}, {self.path}, {self.website})"
-    
+
     def __str__(self) -> str:
         return f"SDModel({self.name}, {self.path}, {self.website})"
 
     @property
     def name(self) -> str:
         return self._name
-    
+
     @property
     def path(self) -> str:
         return self._path
-    
+
     @property
-    def prefix(self) -> str:
-        return self._prefix
-    
+    def positive_prefix(self) -> str:
+        return self._positive_prefix
+
+    @property
+    def negative_prefix(self) -> str:
+        return self._negative_prefix
+
     @property
     def website(self) -> str:
         return self._website
-    
+
 
 _SD_MODELS = {
-    "Anything V4.0": SDModel("Anything V4.0", "xyn-ai/anything-v4.0", "", "https://huggingface.co/xyn-ai/anything-v4.0"),
-    "Arcane Diffusion": SDModel("Arcane Diffusion", "nitrosocke/Arcane-Diffusion", "arcane style", "https://huggingface.co/nitrosocke/Arcane-Diffusion"),
-    "Dreamlike Anime 1.0": SDModel("Dreamlike Anime 1.0", "dreamlike-art/dreamlike-anime-1.0", "photo anime", "https://huggingface.co/dreamlike-art/dreamlike-anime-1.0"),
-    "Dreamlike Diffusion 1.0": SDModel("Dreamlike Diffusion 1.0", "dreamlike-art/dreamlike-diffusion-1.0", "dreamlikeart", "https://huggingface.co/dreamlike-art/dreamlike-diffusion-1.0"),
-    "Dreamlike Photoreal 2.0": SDModel("Dreamlike Photoreal 2.0", "dreamlike-art/dreamlike-photoreal-2.0", "photo", "https://huggingface.co/dreamlike-art/dreamlike-photoreal-2.0"),
-    "Ghibli Diffusion": SDModel("Ghibli Diffusion", "nitrosocke/Ghibli-Diffusion", "ghibli style", "https://huggingface.co/nitrosocke/Ghibli-Diffusion"),
-    "Openjourney": SDModel("Openjourney", "prompthero/openjourney", "mdjrny-v4 style", "https://huggingface.co/prompthero/openjourney"),
-    "SD V1.5": SDModel("Stable Diffusion V1.5", "runwayml/stable-diffusion-v1-5", "", "https://huggingface.co/runwayml/stable-diffusion-v1-5"),
-    "SD V2 Base": SDModel("Stable Diffusion V2 Base", "stabilityai/stable-diffusion-2-base", "", "https://huggingface.co/stabilityai/stable-diffusion-2-base"),
-    "SD V2-1": SDModel("Stable Diffusion V2-1", "stabilityai/stable-diffusion-2-1", "", "https://huggingface.co/stabilityai/stable-diffusion-2-1"),
-    "SD x2 Latent Upscaler": SDModel("Stable Diffusion x2 Latent Upscaler", "stabilityai/sd-x2-latent-upscaler", "", "https://huggingface.co/stabilityai/sd-x2-latent-upscaler"),
-    "SD x4 Upscaler": SDModel("Stable Diffusion x4 Upscaler", "stabilityai/stable-diffusion-x4-upscaler", "", "https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler"),
-    "SDXL Base V1.0": SDModel("Stable Diffusion XL Base V1.0", "stabilityai/stable-diffusion-xl-refiner-1.0", "", "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0"),
-    "SDXL Refiner V1.0": SDModel("Stable Diffusion XL Refiner V1.0", "stabilityai/stable-diffusion-xl-base-1.0", "", "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0"),
-    "Waifu Diffusion": SDModel("Waifu Diffusion", "hakurei/waifu-diffusion", "", "https://huggingface.co/hakurei/waifu-diffusion"),
+    "AbyssOrangeMix3A1B": SDModel(
+        "AbyssOrangeMix3A1B",
+        "stablediffusionapi/abyssorangemix3a1b",
+        "",
+        "",
+        "https://huggingface.co/stablediffusionapi/abyssorangemix3a1b",
+    ),
+    "Anything V4.0": SDModel(
+        "Anything V4.0",
+        "xyn-ai/anything-v4.0",
+        "",
+        "",
+        "https://huggingface.co/xyn-ai/anything-v4.0",
+    ),
+    "Arcane Diffusion": SDModel(
+        "Arcane Diffusion",
+        "nitrosocke/Arcane-Diffusion",
+        "arcane style",
+        "",
+        "https://huggingface.co/nitrosocke/Arcane-Diffusion",
+    ),
+    "CetusMix": SDModel(
+        "CetusMix",
+        "stablediffusionapi/cetusmix",
+        "",
+        "",
+        "https://huggingface.co/stablediffusionapi/cetusmix",
+    ),
+    "Dreamlike Anime 1.0": SDModel(
+        "Dreamlike Anime 1.0",
+        "dreamlike-art/dreamlike-anime-1.0",
+        "photo anime",
+        "",
+        "https://huggingface.co/dreamlike-art/dreamlike-anime-1.0",
+    ),
+    "Dreamlike Diffusion 1.0": SDModel(
+        "Dreamlike Diffusion 1.0",
+        "dreamlike-art/dreamlike-diffusion-1.0",
+        "dreamlikeart",
+        "",
+        "https://huggingface.co/dreamlike-art/dreamlike-diffusion-1.0",
+    ),
+    "Dreamlike Photoreal 2.0": SDModel(
+        "Dreamlike Photoreal 2.0",
+        "dreamlike-art/dreamlike-photoreal-2.0",
+        "photo",
+        "",
+        "https://huggingface.co/dreamlike-art/dreamlike-photoreal-2.0",
+    ),
+    "Ghibli Diffusion": SDModel(
+        "Ghibli Diffusion",
+        "nitrosocke/Ghibli-Diffusion",
+        "ghibli style",
+        "https://huggingface.co/nitrosocke/Ghibli-Diffusion",
+    ),
+    "MeinaMix V11": SDModel(
+        "MeinaMix V11",
+        "Meina/MeinaMix_V11",
+        "",
+        "(worst quality, low quality:1.4), (zombie, sketch, interlocked fingers, comic)",
+        "https://huggingface.co/Meina/MeinaMix_V11",
+    ),
+    "Openjourney": SDModel(
+        "Openjourney",
+        "prompthero/openjourney",
+        "mdjrny-v4 style",
+        "",
+        "https://huggingface.co/prompthero/openjourney",
+    ),
+    "Stable Diffusion V1.5": SDModel(
+        "Stable Diffusion V1.5",
+        "runwayml/stable-diffusion-v1-5",
+        "",
+        "",
+        "https://huggingface.co/runwayml/stable-diffusion-v1-5",
+    ),
+    "Stable Diffusion V2": SDModel(
+        "Stable Diffusion V2",
+        "stabilityai/stable-diffusion-2",
+        "",
+        "",
+        "https://huggingface.co/stabilityai/stable-diffusion-2",
+    ),
+    "Stable Diffusion V2-1": SDModel(
+        "Stable Diffusion V2-1",
+        "stabilityai/stable-diffusion-2-1",
+        "",
+        "",
+        "https://huggingface.co/stabilityai/stable-diffusion-2-1",
+    ),
+    "Stable Diffusion x2 Latent Upscaler": SDModel(
+        "Stable Diffusion x2 Latent Upscaler",
+        "stabilityai/sd-x2-latent-upscaler",
+        "",
+        "",
+        "https://huggingface.co/stabilityai/sd-x2-latent-upscaler",
+    ),
+    "Stable Diffusion x4 Upscaler": SDModel(
+        "Stable Diffusion x4 Upscaler",
+        "stabilityai/stable-diffusion-x4-upscaler",
+        "",
+        "",
+        "https://huggingface.co/stabilityai/stable-diffusion-x4-upscaler",
+    ),
+    "Stable Diffusion XL Base V1.0": SDModel(
+        "Stable Diffusion XL Base V1.0",
+        "stabilityai/stable-diffusion-xl-refiner-1.0",
+        "",
+        "",
+        "https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0",
+    ),
+    "Stable Diffusion XL Refiner V1.0": SDModel(
+        "Stable Diffusion XL Refiner V1.0",
+        "stabilityai/stable-diffusion-xl-base-1.0",
+        "",
+        "",
+        "https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0",
+    ),
+    "Waifu Diffusion": SDModel(
+        "Waifu Diffusion",
+        "hakurei/waifu-diffusion",
+        "",
+        "",
+        "https://huggingface.co/hakurei/waifu-diffusion",
+    ),
 }
 
 
@@ -71,5 +187,5 @@ def get_sd_model_names() -> tuple[str]:
 
 def get_sd_model(name: str) -> SDModel:
     if (sd_model := _SD_MODELS.get(name)) is None:
-        raise KeyError(f"Model \"{name}\" not found.")
+        raise KeyError(f'Model "{name}" not found.')
     return sd_model
